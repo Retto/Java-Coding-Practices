6e120e25514ef6e363dea52d4bd889264884fc46
Retto <resulsahin@gmail.com>
2022-11-08 17:09:38 +0300

    add new problem

---
+++ src/main/java/com/retto/practices6/README.md
@@ -0,0 +1,9 @@
+# Problem 6
+Here is the problem. 
+
+Time complexity is O(n)
+Space complexity is O(n)
+
+
+
+
---
+++ src/main/java/com/retto/practices6/Solution.java
@@ -0,0 +1,40 @@
+package com.retto.practices6;
+
+public class Solution {
+
+	private static int max(int a, int b) {
+		return a > b ? a : b;
+	}
+
+	private static boolean check(int[] nums) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] > nums[i + 1]) {
+				if (i + 2 < nums.length && nums[i + 2] >= nums[i])
+					nums[i + 1] = nums[i];
+				else {
+					if (i + 2 == nums.length) {
+						nums[i + 1] = max(nums[i], nums[i + 1]);
+					} else {
+						nums[i] = nums[i + 1];
+					}
+				}
+
+				break;
+			}
+		}
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (!(nums[i] <= nums[i + 1])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(check(new int[] { 13, 4, 7 }));
+
+		System.out.println(check(new int[] { 5, 1, 3, 2, 5 }));
+	}
+}
